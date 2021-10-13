4402d6186089aa8175239fa7f7bb032060a22b89
Christopher Liebe <carlchristopherliebe@gmail.com>
2021-10-13 10:59:50 -0700

    Quiz game complete with json file. Translations and compatibility with foreign languages needed.

--- app/src/main/java/com/example/quizgame/MainActivity.kt
+++ app/src/main/java/com/example/quizgame/MainActivity.kt
@@ -22,35 +22,27 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        wireWidgets()
 
-        // reading the json from the raw folder
-
-        // step 1: open the raw resource as an InputStream
-        // R.raw.questions --> R is the Resources class, raw is folder, questions is the file
         val inputStream = resources.openRawResource(R.raw.questions)
-        // step 2: use a buffered reader on the inputstream tp read the text into a string
         val jsonText = inputStream.bufferedReader().use {
-            // the last line of the use function is returned
             it.readText()
         }
-        Log.d(TAG, "onCreate: $jsonText")
 
+        Log.d(TAG, "onCreate: $jsonText")
         val gson = Gson()
         val type = object : TypeToken<List<Question>>() { }.type
         val questions = gson.fromJson<List<Question>>(jsonText, type)
         Log.d(TAG, "onCreate: \n${questions.toString()}")
 
+
         val quiz = Quiz(questions)
 
-        wireWidgets()
-
         fun presentScore() {
-            questionNumber.text = "Score: ${quiz.score}"
             leftButton.visibility = GONE
             rightButton.visibility = GONE
             if (quiz.score < 61) {
-                question.text =
-                    "You are nowhere near being a gamer... Why are you taking this quiz?"
+                question.text = "@string/main_score"
             } else if (quiz.score < 101) {
                 question.text = "You are a normal human being..."
             } else if (quiz.score < 151) {
@@ -72,73 +64,10 @@ class MainActivity : AppCompatActivity() {
                 leftButton.text = quiz.questions[quiz.questionNumber].LeftAnswer
                 rightButton.text = quiz.questions[quiz.questionNumber].RightAnswer
                 question.text = quiz.questions[quiz.questionNumber].Question
-                questionNumber.text = "${quiz.questionNumber + 1}"
             }
         }
 
         updateScreen()
-
-        // use gson to convert the jsonText into a List<Question>
-        // https://medium.com/@hissain.khan/parsing-with-google-gson-library-in-android-kotlin-7920e26f5520
-        // check the section called "Parsing between a Collection, List, or Array"
-        //Log the list of questions and see if your Question objects all appear correct
-
-
-        //wireWidgets()
-
-        /*val questions = MutableList(1) { Question("Are you an introvert or an extrovert?", "Introvert", "Extrovert", 20, 10 ) }
-        questions.add(Question("Do you like warm or cold colors?", "Warm", "Cold", 10, 20))
-        questions.add(Question("Are you a night owl or a morning owl?", "Night Owl", "Morning Owl", 40, 5))
-        questions.add(Question("Do you procrastinate your work?", "Yes", "No", 30, 5))
-        questions.add(Question("Do you like Math/Sciences or English/History/Arts?", "Math/Sciences", "English/History/Arts", 5, 5))
-        questions.add(Question("How long do you spend on the computer everyday?", "Less than an hour", "More than an hour", 5, 40))
-        questions.add(Question("Do you like traveling?", "Yes", "No", 5, 25))
-        questions.add(Question("Do you clean your room?", "Yes", "No", 10, 20))
-        questions.add(Question("Do you like school?", "Yes", "No", 0, 30))
-        questions.add(Question("Do you prefer the Summer/Spring or Winter/Fall", "Summer/Spring", "Winter/Fall", 5, 5))
-
-        leftButton.text = questions[i].buttonLeft
-        rightButton.text = questions[i].buttonRight
-        question.text = questions[i].question
-        questionNumber.text = "Question #1"
-
-        leftButton.setOnClickListener {
-            if (i < 9) {
-                score += questions[i].leftPoints
-                i++
-                leftButton.text = questions[i].buttonLeft
-                rightButton.text = questions[i].buttonRight
-                question.text = questions[i].question
-                questionNumber.text = "Question #${i + 1}"
-            }
-            else {
-                score += questions[i].leftPoints
-                presentScore()
-            }
-        }
-
-        rightButton.setOnClickListener {
-            if (i < 9) {
-                score += questions[i].rightPoints
-                i++
-                leftButton.text = questions[i].buttonLeft
-                rightButton.text = questions[i].buttonRight
-                question.text = questions[i].question
-                questionNumber.text = "Question #${i + 1}"
-            }
-            else {
-                score += questions[i].rightPoints
-                presentScore()
-            }
-        }
-
-
-
-    }*/
-
-
-
-
 
         leftButton.setOnClickListener() {
             quiz.left()
@@ -149,17 +78,6 @@ class MainActivity : AppCompatActivity() {
             quiz.right()
             updateScreen()
         }
-        // answer1Button.setonCLicklistener {
-        // any quiz - related actions -- scorekeeping, checking if answers are right or wrong, keeping track of which question we're on, if there are more questions remaining all duties of the Quiz class
-
-        // MainActivity is in charge of the UI and passing information to and from the Quiz and class
-
-        // Tell the quiz what was clicked on and let the quiz determine if the answer was right or wrong
-        // update the score text view based on the current score
-        //  ask the quiz if there are more questions, and if there are set the question text and button text to the new question and answer choices
-
-        // if there aren't any more questions, then hide a bunch of UI and give the final score }
-
     }
 
     fun wireWidgets() {
--- app/src/main/java/com/example/quizgame/Quiz.kt
+++ app/src/main/java/com/example/quizgame/Quiz.kt
@@ -13,12 +13,4 @@ data class Quiz (var questions: List<Question>) {
         score += questions[questionNumber].RightPoints
         questionNumber++
     }
-
-    /*fun checkAnswer(answer: Boolean) {
-        //  check if the answer in the param matches the answer of the current question
-        //if it does
-        score++
-        // else
-        score--
-    }*/
 }
